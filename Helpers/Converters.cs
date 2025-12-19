7f46d6b6bdc633b2102031aaab9760bd3e98406b
Peter Egunjobi <masterpdon@gmail.com>
2025-12-19 01:38:22 +0100

    Added EnumToVisibilityConverter

---
+++ Helpers/Converters.cs
@@ -0,0 +1,25 @@
+﻿using CompressMediaPage;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinUIShared.Helpers
+{
+    public class EnumToVisibilityConverter : IValueConverter
+    {
+        public CompressionMethod Enum { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not CompressionMethod enumValue) return Visibility.Collapsed;
+            return enumValue.Equals(Enum) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
