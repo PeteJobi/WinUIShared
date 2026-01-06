50b796749c8ccf1476218d4d94a74be8491c5061
Peter Egunjobi <masterpdon@gmail.com>
2026-01-06 19:14:34 +0100

    Added ComboBox extension to fix layout bug

---
+++ Controls/ComboBoxEx.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Windows.Foundation;
+
+namespace WinUIShared.Controls
+{
+    //This ComboBox extension is used to fix the issue where the ComboBox shifts when opened/closed
+    public class ComboBoxEx : ComboBox
+    {
+        double _cachedWidth;
+
+        protected override void OnDropDownOpened(object e)
+        {
+            Width = _cachedWidth;
+
+            base.OnDropDownOpened(e);
+        }
+
+        protected override void OnDropDownClosed(object e)
+        {
+            Width = double.NaN;
+
+            base.OnDropDownClosed(e);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var baseSize = base.MeasureOverride(availableSize);
+
+            if (baseSize.Width != 64)
+                _cachedWidth = baseSize.Width;
+
+            return baseSize;
+        }
+    }
+}
