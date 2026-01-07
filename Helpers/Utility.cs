f611547b7462eb827d15a9c4a6d3f1a674a9d778
Peter Egunjobi <masterpdon@gmail.com>
2026-01-07 19:32:50 +0100

    Added Utility class for shared methods. Added method to find child by name.

---
+++ Helpers/Utility.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinUIShared.Helpers
+{
+    public static class Utility
+    {
+        public static DependencyObject? FindChildElementByName(this DependencyObject tree, string sName)
+        {
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(tree); i++)
+            {
+                var child = VisualTreeHelper.GetChild(tree, i);
+                if (child != null && ((FrameworkElement)child).Name == sName)
+                    return child;
+                var childInSubtree = FindChildElementByName(child, sName);
+                if (childInSubtree != null)
+                    return childInSubtree;
+            }
+            return null;
+        }
+    }
+}
